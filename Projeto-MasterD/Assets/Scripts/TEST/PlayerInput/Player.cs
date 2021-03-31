ac7cd263ac561fc57ccd6f0a5f6536ca90a42352
Jorge Silva <jorgemcs14@gmail.com>
2021-04-01 00:57:47 +0100

    WIP: Player Controller
    Trying to understand the new Player Controller System

---
+++ Projeto-MasterD/Assets/Scripts/TEST/PlayerInput/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour {
+
+    public Vector2 normalInput;
+    public Vector2 newInput;
+    private Animator anim;
+    public float dumpTemp = 0.25f;
+    public float speed = 1f;
+
+
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        normalInput.x = Input.GetAxis("Horizontal");
+        normalInput.y = Input.GetAxis("Vertical");
+    }
+
+    public void OnMove(InputValue t) {
+
+        var value = t.Get<Vector2>();
+        float vX = 0f; ;
+        float vY = 0f;
+        if (value.x > 0) { vX += 1f; }
+        else if(value.x < 0) { vX -=1f; }
+
+        vX = Mathf.Clamp(vX, -1, 1);
+
+        if(value.y > 0) { vY += 1f; }
+        else if(value.y < 0) { vY -= 1f; }
+
+        newInput.x = vX;
+        newInput.y = vY;
+
+        anim.SetFloat("horizontal", vX);
+        anim.SetFloat("vertical", vY);
+
+    }
+
+    public void OnInteraction() {
+        Debug.Log("OH YEAH");
+    }
+
+}
---
+++ Projeto-MasterD/Assets/Scripts/TEST/ui/uiTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiTest : MonoBehaviour {
+
+    public void LoadWithLoader() {
+        GameManager.ChangeScene(1, true);
+    }
+
+    public void LoadWithoutLoader() {
+        GameManager.ChangeScene(1, false);
+    }
+
+}
